7a26b26c03961d16f4fd24129fe2a909412bfe67
Matt Hammond <matt@talkthinkdo.co.uk>
2015-06-10 12:24:55 +0100

    Add validation tag helpers

---
+++ src/baymax/TagHelpers/ValidationToggleTagHelper.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.Rendering;
+using Microsoft.AspNet.Mvc.TagHelpers;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace baymax.TagHelpers
+{
+    [TargetElement("div", Attributes = ValidationForAttributeName)]
+    public class ValidationClassNameTagHelper : TagHelper
+    {
+        private const string ValidationForAttributeName = "asp-validation-add-class-for";
+        private const string ClassNameAttributeName = "asp-validation-class-name";
+
+        [HtmlAttributeName(ValidationForAttributeName)]
+        public ModelExpression For { get; set; }
+
+        [HtmlAttributeName(ClassNameAttributeName)]
+        public string ClassName { get; set; }
+
+        [Activate]
+        protected internal ViewContext Context { get; set; }
+
+        internal static string GetFullHtmlFieldName(ViewContext viewContext, string expression)
+        {
+            var fullName = viewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(expression);
+            return fullName;
+        }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var fullName = GetFullHtmlFieldName(Context, For.Name);
+            if (string.IsNullOrEmpty(fullName))
+            {
+                throw new ArgumentException();
+            }
+
+            var formContext = Context.ClientValidationEnabled ? Context.FormContext : null;
+            if (!Context.ViewData.ModelState.ContainsKey(fullName) && formContext == null)
+            {
+                return;
+            }
+
+            ModelState modelState;
+            var tryGetModelStateResult = Context.ViewData.ModelState.TryGetValue(fullName, out modelState);
+            var modelErrors = tryGetModelStateResult ? modelState.Errors : null;
+
+            ModelError modelError = null;
+            if (modelErrors != null && modelErrors.Count != 0)
+            {
+                modelError = modelErrors.FirstOrDefault(m => !string.IsNullOrEmpty(m.ErrorMessage)) ?? modelErrors[0];
+            }
+
+            if (modelError != null)
+            {
+                var tag = Context.ValidationMessageElement;
+
+                var tagBuilder = new TagBuilder(tag);
+
+                tagBuilder.AddCssClass(ClassName);
+                var childContent = await context.GetChildContentAsync();
+                output.Content.SetContent(childContent);
+
+                output.MergeAttributes(tagBuilder);
+            }
+
+
+        }
+    }
+
+    [TargetElement("div", Attributes = ValidationAttributeName)]
+    public class ValidationToggleTagHelper : TagHelper
+    {
+        private const string ValidationAttributeName = "asp-validation-toggle";
+
+        [Activate]
+        protected internal ViewContext Context { get; set; }
+
+        internal static string GetFullHtmlFieldName(ViewContext viewContext, string expression)
+        {
+            var fullName = viewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(expression);
+            return fullName;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var formContext = Context.ClientValidationEnabled ? Context.FormContext : null;
+            if (Context.ViewData.ModelState.IsValid)
+            {
+                output.SuppressOutput();
+            }
+
+
+
+        }
+    }
+}
